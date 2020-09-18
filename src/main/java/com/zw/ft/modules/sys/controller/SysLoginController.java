44ca50aa6ce63c2beed009f57b531d68531bc528
Oliver <gelutou@163.com>
2020-09-18 16:18:06 +0800

    测试远程交互02

--- src/main/java/com/zw/ft/modules/sys/controller/SysLoginController.java
+++ src/main/java/com/zw/ft/modules/sys/controller/SysLoginController.java
@@ -1,12 +1,10 @@
 package com.zw.ft.modules.sys.controller;
 
-import com.zw.ft.common.utils.R;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
+import com.zw.ft.modules.sys.entity.SysUserEntity;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @ClassName SysLoginController
@@ -17,6 +15,11 @@ import java.util.Map;
  **/
 @RestController
 @RequestMapping(value = "/sys/login")
+@Api(value = "用户接口")
 public class SysLoginController {
-
+    @ApiOperation(value = "用户控制")
+    @GetMapping("/get_user")
+    public String hello(@ApiParam(value = "用户实体") String name){
+        return name;
+    }
 }
--- src/main/java/com/zw/ft/modules/sys/entity/SysUserEntity.java
+++ src/main/java/com/zw/ft/modules/sys/entity/SysUserEntity.java
@@ -2,6 +2,8 @@ package com.zw.ft.modules.sys.entity;
 
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.zw.ft.common.base.BaseEntity;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModelProperty;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotEmpty;
@@ -15,6 +17,7 @@ import java.util.List;
  * @Date 2020/9/9 16:50
  * @Version 1.0
  **/
+@Api(value = "系统用户实体")
 @TableName(value = "sys_user")
 public class SysUserEntity extends BaseEntity {
     private static final long serialVersionUID = -127682095331146479L;
@@ -25,27 +28,32 @@ public class SysUserEntity extends BaseEntity {
     @NotEmpty(message = "登录账号不能为空")
     @Length(min = 5,max = 16,message = "账号长度为 5-16 位")
     @Pattern(regexp = "^[A-Za-z0-9]+$",message = "账号内容只能是数字和字母")
+    @ApiModelProperty(value = "用户名")
     private String userName;
     /**
      *@description: 密码
      */
     @NotEmpty(message = "密码不能为空")
     @Length(min = 5,max = 16,message = "密码长度为 5-16 位")
+    @ApiModelProperty(value = "密码")
     private String password;
     /**
      *@description: 姓名
      */
     @NotEmpty(message = "姓名不能为空")
+    @ApiModelProperty(value = "真名")
     private String realName;
     /**
      *@description: 性别 0 男 1 女
      */
     @NotEmpty(message = "性别不能为空")
+    @ApiModelProperty(value = "性别，0 男 1 女")
     private Integer gender;
 
     /**
      *@description: 角色集合
      */
+    @ApiModelProperty(value = "角色")
     private List<SysRoleEntity> roles;
 
     public String getUserName() {
