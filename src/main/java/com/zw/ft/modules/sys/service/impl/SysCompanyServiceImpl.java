31977d9b78a1aee264d4e94756789dcf7486230a
Oliver <gelutou@163.com>
2020-10-13 15:37:14 +0800

    格式化前台参数

--- src/main/java/com/zw/ft/modules/sys/service/impl/SysCompanyServiceImpl.java
+++ src/main/java/com/zw/ft/modules/sys/service/impl/SysCompanyServiceImpl.java
@@ -1,5 +1,11 @@
 package com.zw.ft.modules.sys.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.zw.ft.common.base.Constant;
+import com.zw.ft.common.utils.FormatUtil;
+import com.zw.ft.common.utils.QueryUtil;
+import com.zw.ft.common.utils.R;
 import com.zw.ft.modules.sys.entity.SysCompany;
 import com.zw.ft.modules.sys.repository.SysCompanyMapper;
 import com.zw.ft.modules.sys.service.SysCompanyService;
@@ -8,6 +14,7 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -34,5 +41,55 @@ public class SysCompanyServiceImpl extends ServiceImpl<SysCompanyMapper, SysComp
     @Override
     public List<SysCompany> getFuzzy(String username, String shortName) {
         return sysCompanyMapper.getFuzzy(username,shortName);
+    }
+
+    @Override
+    public Page<SysCompany> getComPage(Map<String, Object> params) {
+        Page<SysCompany> page = new QueryUtil<SysCompany>(params).getPage();
+        QueryWrapper<SysCompany> queryWrapper = new QueryWrapper<>();
+
+        String name = FormatUtil.isSelectKey("name", params);
+        if(Constant.TRUE.equals(name)){
+            queryWrapper.like("com_name",params.get("name"));
+        }else if("".equals(name)){
+            queryWrapper.like("com_name","");
+        }
+
+        String shortName = FormatUtil.isSelectKey("shortName", params);
+        if(Constant.TRUE.equals(shortName)){
+            queryWrapper.like("short_com_name",params.get("shortName"));
+        }else if("".equals(shortName)){
+            queryWrapper.like("short_com_name","");
+        }
+
+        String code = FormatUtil.isSelectKey("code", params);
+        if(Constant.TRUE.equals(code)){
+            queryWrapper.like("com_code",params.get("code"));
+        }else if("".equals(code)){
+            queryWrapper.like("com_code","");
+        }
+
+        String startDateTime = FormatUtil.isSelectKey("startDateTime", params);
+        if(Constant.TRUE.equals(startDateTime)){
+            queryWrapper.ge("created_time",params.get("startDateTime"));
+        }else if("".equals(startDateTime)){
+            queryWrapper.ge("created_time","");
+        }
+
+        String endDateTime = FormatUtil.isSelectKey("endDateTime", params);
+        if(Constant.TRUE.equals(endDateTime)){
+            queryWrapper.le("created_time",params.get("endDateTime"));
+        }else if("".equals(endDateTime)){
+            queryWrapper.le("created_time","");
+        }
+
+        String mobile = FormatUtil.isSelectKey("mobile", params);
+        if(Constant.TRUE.equals(mobile)){
+            queryWrapper.like("mobile",params.get("mobile"));
+        }else if("".equals(mobile)){
+            queryWrapper.like("mobile","");
+        }
+        queryWrapper.orderByAsc("updated_time");
+        return sysCompanyMapper.selectPage(page,queryWrapper);
     }
 }
