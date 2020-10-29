933256d372b9e92321f1a74f7dce021cb2ff7899
savior <zsh24638@163.com>
2020-10-29 09:21:29 +0800

    'sysbank模糊查询提交'

--- src/main/java/com/zw/ft/modules/sys/repository/SysBankMapper.java
+++ src/main/java/com/zw/ft/modules/sys/repository/SysBankMapper.java
@@ -1,6 +1,6 @@
 package com.zw.ft.modules.sys.repository;
 
-import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.zw.ft.modules.sys.entity.SysBank;
@@ -22,10 +22,8 @@ public interface SysBankMapper extends BaseMapper<SysBank> {
     @Select("SELECT su.*,scy.com_name,us.username FROM sys_bank su " +
             "LEFT JOIN sys_company scy ON su.com_id = scy.ID " +
             "LEFT JOIN sys_user us ON us.ID=su.CREATED_BY " +
-            "WHERE su.deleted=0 AND (su.BANK_NAME LIKE '%%' " +
-            "AND scy.com_name LIKE '%%' AND su.BANK_ACCOUNT LIKE '%%' " +
-            "AND su.ACCOUNT_STYLE LIKE '%%' AND su.ACCOUNT_TYPE LIKE '%%')")
-    Page<SysBank> querySysBankPageByComId(Page<SysBank> page,@Param("ew") Wrapper<SysBank> queryWrapper);
+            "WHERE su.deleted=0 ${ew.sqlSegment} ")
+    Page<SysBank> querySysBankPageByComId(Page<SysBank> page, @Param("ew") QueryWrapper<SysBank> queryWrapper);
 
 
 }
