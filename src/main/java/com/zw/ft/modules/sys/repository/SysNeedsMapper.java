21921386b9da16eeaac0926944aedcd30214523d
Oliver <gelutou@163.com>
2020-10-30 10:41:18 +0800

    去掉多数据源

---
+++ src/main/java/com/zw/ft/modules/sys/repository/SysNeedsMapper.java
@@ -0,0 +1,16 @@
+package com.zw.ft.modules.sys.repository;
+
+import com.zw.ft.modules.sys.entity.SysNeeds;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  系统需求表 Mapper 接口
+ * </p>
+ *
+ * @author Oliver
+ * @since 2020-10-29
+ */
+public interface SysNeedsMapper extends BaseMapper<SysNeeds> {
+
+}
---
+++ src/main/java/com/zw/ft/modules/sys/service/impl/SysNeedsServiceImpl.java
@@ -0,0 +1,20 @@
+package com.zw.ft.modules.sys.service.impl;
+
+import com.zw.ft.modules.sys.entity.SysNeeds;
+import com.zw.ft.modules.sys.repository.SysNeedsMapper;
+import com.zw.ft.modules.sys.service.SysNeedsService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  系统需求表 服务实现类
+ * </p>
+ *
+ * @author Oliver
+ * @since 2020-10-29
+ */
+@Service
+public class SysNeedsServiceImpl extends ServiceImpl<SysNeedsMapper, SysNeeds> implements SysNeedsService {
+
+}
