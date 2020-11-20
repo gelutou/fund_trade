3d911df7ef8fc1dfa76e53e95129dc7abc840a5d
Oliver <gelutou@163.com>
2020-11-20 16:51:58 +0800

    优化代码

--- src/main/java/com/zw/ft/common/base/BaseController.java
+++ src/main/java/com/zw/ft/common/base/BaseController.java
@@ -1,5 +1,9 @@
 package com.zw.ft.common.base;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.zw.ft.modules.sys.entity.SysCompany;
+import com.zw.ft.modules.sys.entity.SysUser;
+import org.apache.shiro.SecurityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -18,8 +22,36 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class BaseController {
 
     protected Logger logger = LoggerFactory.getLogger(getClass());
+
     @GetMapping(value = "/{url}")
     public String redirect(@PathVariable("url") String url){
         return url;
     }
+
+    protected SysUser getUser() {
+        return (SysUser) SecurityUtils.getSubject().getPrincipal();
+    }
+
+    protected String getUserName() {
+        SysUser sysUer = getUser();
+        if(sysUer==null){
+            return "admin";
+        }
+        return sysUer.getUsername();
+    }
+
+    protected Long getUserId() {
+        SysUser sysUer = getUser();
+        if(sysUer==null){
+            return -1L;
+        }
+        return sysUer.getId();
+    }
+
+    /*protected SysCompany getCompany(){
+        Long userId = getUserId();
+        QueryWrapper<SysCompany> companyInfoEntityEntityWrapper = new EntityWrapper<>();
+        companyInfoEntityEntityWrapper.eq("user_id",userId);
+        return companyInfoService.selectOne(companyInfoEntityEntityWrapper);
+    }*/
 }
--- src/main/java/com/zw/ft/modules/sys/entity/RelUserDepartment.java
+++ src/main/java/com/zw/ft/modules/sys/entity/RelUserDepartment.java
@@ -1,10 +1,11 @@
 package com.zw.ft.modules.sys.entity;
 
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.*;
 import com.zw.ft.common.base.BaseEntity;
-import com.baomidou.mybatisplus.annotation.Version;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import javax.validation.constraints.NotEmpty;
 
 /**
  * <p>
@@ -14,12 +15,17 @@ import lombok.EqualsAndHashCode;
  * @author Oliver
  * @since 2020-11-03
  */
-@EqualsAndHashCode(callSuper = true)
 @TableName(value = "rel_user_department")
 @Data
-public class RelUserDepartment extends BaseEntity {
+public class RelUserDepartment {
 
     private static final long serialVersionUID = 7100356565731267027L;
+
+    /**
+     *@description: 主键ID
+     */
+    @TableId
+    private Long id;
     /**
      * 人员 人员ID
      */
@@ -29,4 +35,31 @@ public class RelUserDepartment extends BaseEntity {
      * 部门 部门ID
      */
     private Integer deptId;
+
+    /**
+     *@description: 创建人ID
+     */
+
+    @TableField(fill = FieldFill.INSERT)
+    private Long createdBy;
+
+    /**
+     *@description: 创建时间
+     */
+
+    @NotEmpty(message = "创建时间不能为空")
+    private String createdTime;
+
+    /**
+     *@description: 修改人ID
+     */
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Long updatedBy;
+
+    /**
+     *@description: 修改时间
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private String updatedTime;
 }
